f79302e801dcf66b0000d0e905c0b5bb3bdf411f
Nevendra <nevendra@live>
2016-08-30 15:18:32 -0400

    Updating gh-pages from master branch via GitBot

--- JS/guide.js
+++ JS/guide.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+	var windowWidth = $(window).width();
 	
 	function toggleNav() {
 		$('nav').slideToggle()
@@ -18,6 +20,24 @@ $(document).ready(function(){
         $(this).parent().parent().children('.dropdown').slideToggle();
         console.log($(this).parent().parent().children('.dropdown'));
     });
+
+
+	function imageFlip(){
+		if(windowWidth > 768){
+			$(".flipper").mouseenter(function(){
+	    		$(".flip-container").toggleClass("flip")
+	    	})
+	    	$(".flipper").mouseleave(function(){
+	    		$(".flip-container").toggleClass("flip")
+	    	})
+
+		} else {
+			$(".flipper").click(function(){
+	    	$(".flip-container").toggleClass("flip")
+	    	})
+		}
+	}///end function imageFlip
+	imageFlip();
 	
 
 });
