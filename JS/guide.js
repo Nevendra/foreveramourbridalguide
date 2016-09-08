b85ace78059d40251d0b7de07409bcb3dba86e17
Nevendra <nevendra@live>
2016-09-08 12:39:11 -0400

    Updating gh-pages from master branch via GitBot

--- JS/guide.js
+++ JS/guide.js
@@ -22,18 +22,58 @@ $(document).ready(function(){
     });
 
 
-	function imageFlip(){
-		$('.flip').click(function(){
-        if($(this).find('.card').hasClass('flipped')){
-        	$(this).find('.card').removeClass('flipped');
-        } else {
-        	$(this).find('.card').addClass('flipped');
-        }
- 	});
+	// function imageFlip(){
+	// 	$('.flip').click(function(){
+ //        if($(this).find('.card').hasClass('flipped')){
+ //        	$(this).find('.card').removeClass('flipped');
+ //        } else {
+ //        	$(this).find('.card').addClass('flipped');
+ //        }
+ // 	});
 
 
-	}///end function imageFlip
-	imageFlip();
-	
+	// }///end function imageFlip
+	// imageFlip();
+	var ckbox = $('#squaredTwo');
+
+	$('input').on('click',function () {
+			var box = $(this).parent().parent().parent();
+	        if ($(this).is(':checked') && $(this).hasClass('flipBox')) {
+		        if(box.find('.card').hasClass('flipped')){
+		        	box.find('.card').removeClass('flipped');
+		        } else {
+		        	box.find('.card').addClass('flipped');
+		        	$(this).attr('disabled', true)
+		        }
+	        }
+	    });
+
+	$('input').on('click',function () {
+			var box = $(this).parent().parent().parent();
+	        if ($(this).is(':checked') && $(this).hasClass('slideBox')) {
+		        if(box.find('.imgGallery').hasClass('slideImg')){
+		        	box.find('.imgGallery').removeClass('slideImg');
+		        } else {
+		        	box.find('.imgGallery').addClass('slideImg');
+		        	if(box.find('.imgGallery').hasClass('slideImg')) {
+		        		$(box.find('.imgGallery')).animate({height: '700px'}, 'slow')
+		        		$(box.find('.slide')).animate({height: '800px'}, 'slow', showImgGallery(box));
+		        	}
+		        	$(this).attr('disabled', true)
+		        }
+	        }
+	    });
+
+	function showImgGallery(box) {
+		console.log("hey")
+		$(box.find('.imgDisplay')).fadeIn(3000);
+	}
+
+	$(".thumbnail").click(function(){
+			var thumb = $(this).parent().parent().parent()
+			$(thumb.find('.bigImg').children()).attr("src", $(this).attr("src"));
+			console.log($(this).attr("src"));
+		});
+
 
 });
