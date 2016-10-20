1b69e0b63c1f52a4e9c25491102870dcb7958c1b
Nevendra Seonarain <nevendra@live.com>
2016-10-20 12:34:40 -0400

    text color and animation speed change

--- JS/guide.js
+++ JS/guide.js
@@ -98,17 +98,17 @@ $('.imgArrows').click(function(){
 
 				$(".transitionAnimation").css("display", "block");
 					function fades(){
-						$(".animationImage").fadeIn(2000, function(){
-							$(".animationImage").fadeOut(3000, function(){
+						$(".animationImage").fadeIn(1000, function(){
+							$(".animationImage").fadeOut(1000, function(){
 								transitionToNextStep();
-								$('.animationLeft').animate({left: "-50%"}, 500);
-								$('.animationRight').animate({right: "-50%"}, 500);
-								$(".transitionAnimation").stop().fadeOut(1000);
+								$('.animationLeft').animate({left: "-50%"}, 300);
+								$('.animationRight').animate({right: "-50%"}, 300);
+								$(".transitionAnimation").stop().fadeOut(500);
 							});/////// end of fade out
 						});/////end of fadeIn
 					}//// end of fades
-					$('.animationRight').animate({right: "0%"}, 500);
-					$('.animationLeft').animate({left: "0%"}, 500, fades);
+					$('.animationRight').animate({right: "0%"}, 300);
+					$('.animationLeft').animate({left: "0%"}, 300, fades);
 
 		})
 
